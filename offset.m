8b19559d66066891e2c7dbc6724d2d7d72a47438
kentuckybigfoot <kentuckybigfoot@gmail.com>
2016-05-18 11:40:07 -0500

    - Added force setting. Still in works.

--- offset.m
+++ offset.m
@@ -1,11 +1,16 @@
-function [ xOffset ] = offset( x,sensitivity )
+function [ xOffset ] = offset( x,sensitivity,force)
 %normalize Summary of this function goes here
 %   Detailed explanation goes here
     if nargin == 1
         sensitivity = 25; %range end for offset
+        force = [];
     end
     
-    offset = mean(x(1:sensitivity));
+    if isempty(force)
+        offset = mean(x(1:sensitivity));
+    else
+        offset = force;
+    end
     
     xOffset = x - offset;
 end
