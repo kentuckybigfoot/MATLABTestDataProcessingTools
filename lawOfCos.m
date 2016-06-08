1fbd2d0f0339cbe8229d1fb158acce246290f26e
Christopher Kerner <CLK0032@auburn.edu>
2016-06-08 12:17:16 -0500

    Vectorized this function to allow multiple inputs and outputs.

--- lawOfCos.m
+++ lawOfCos.m
@@ -1,4 +1,4 @@
-function [ alpha beta gamma ] = lawOfCos(dist)
+function [ alpha, beta, gamma ] = lawOfCos(dist)
 % lawOfCos Law of cosines function
 % Uses law of cosines to determine angles of triangles formed using
 % wirepots. 
@@ -21,9 +21,8 @@ function [ alpha beta gamma ] = lawOfCos(dist)
 %                  A /_?______ß_\ B
 %                          c
 
-alpha = acos((dist(1)^2 - dist(2)^2 - dist(3)^2)/(-2*dist(2)*dist(3)));
-beta  = acos((dist(2)^2 - dist(1)^2 - dist(3)^2)/(-2*dist(1)*dist(3)));
-gamma = acos((dist(3)^2 - dist(1)^2 - dist(2)^2)/(-2*dist(1)*dist(2)));
+alpha(:,1) = acos((dist(:,1).^2 - dist(:,2).^2 - dist(:,3).^2)./(-2*dist(:,2).*dist(:,3)));
+beta(:,1)  = acos((dist(:,2).^2 - dist(:,1).^2 - dist(:,3).^2)./(-2*dist(:,1).*dist(:,3)));
+gamma(:,1) = acos((dist(:,3).^2 - dist(:,1).^2 - dist(:,2).^2)./(-2*dist(:,1).*dist(:,2)));
 
 end
-
