98c56c3a375d022a39e9afadce8babcb3743d1dd
kentuckybigfoot <kentuckybigfoot@gmail.com>
2018-05-23 03:45:37 -0500

    Modernized and improved code. Added documentation.

--- libCommonFxns/getShearTab.m
+++ libCommonFxns/getShearTab.m
@@ -1,24 +1,40 @@
 function [ ProcessShearTab ] = getShearTab( ProcessFileName )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+%getShearTab Attempts through filename validation to obtain the shear tab experimentally evaluated 
+%   Proper formmating calls for signals filenames to be formatted:
+%       FS Testing - ST3 - Test 1 - 08-24-16.mat
+%   Locates the 'STX' portion to return X. Attempts to allow some wiggle room by also using some light regexp.
+%
+%   Copyright 2017-2018 Christopher L. Kerner.
+%
 
-    ProcessFileNameTmp = strtrim(strsplit(ProcessFileName,'-'));
-
-    ProcessFileNameTmp2 = contains(ProcessFileNameTmp,'ST');
-
-    ProcessFileNameTmpIdx = find(ProcessFileNameTmp2 == 1);
-
-    switch char(ProcessFileNameTmp(ProcessFileNameTmpIdx))
-        case 'ST1'
-            ProcessShearTab = 1;
-        case 'ST2'
-            ProcessShearTab = 2;
-        case 'ST3'
-            ProcessShearTab = 3;
-        case 'ST4'
-            ProcessShearTab = 4;
-        otherwise
-            error('Could not determine which shear tab data record pertains to');
+    % First attempt to obtain value through string manipulation
+    filenSTstrSplit = strtrim(strsplit(ProcessFileName,'-'));
+    STStrfind = contains(filenSTstrSplit, {'ST1','ST2','ST3','ST4'}, 'IgnoreCase', true);
+    idxSTStr = find(STStrfind == 1);
+    
+    % Now also try through regexp
+    STRegexpResults = regexp(ProcessFileName, '[a-zA-Z]{2}\d{1}', 'match');
+    
+    % Create a logical array and find location of any logical true
+    ResLogic = [size(STRegexpResults)==1, any(idxSTStr)];
+    
+    if all(ResLogic)
+        % Both methods found result(s). Format the value, and return it Regardless of if they are identical, err on the side
+        % of caution with regexp in the event that they did not.
+        ProcessShearTab = str2double(strrep(STRegexpResults, 'ST', ''));
+        return     
+    elseif any(idxResLogic)
+        % Only one returned results, attempt to clean and return it.
+        if ResLogic(1)
+            ProcessShearTab = str2double(strrep(STRegexpResults, 'ST', ''));
+            return
+        else
+            STStrRes = char(filenSTstrSplit(idxSTStr));
+            ProcessShearTab = str2double(strrep(STStrRes, 'ST', ''));
+            return
+        end
+    else
+        error('Could not determine a valid shear tab value using filename.')
     end
 end
 
